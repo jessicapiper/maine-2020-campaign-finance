4b34a59942896e151800dfb09dfb03775b88150f
jessica piper <jpiper231@gmail.com>
2020-01-08 16:03:03 -0500

    added more graphs

--- src/_scripts/_charts.js
+++ src/_scripts/_charts.js
@@ -2,12 +2,12 @@ var d3 = require("d3");
 
 function totalsChart(el, fieldname) {
 
-var margin = {top: 20, right:20, bottom:80, left:60};
+var margin = {top: 60, right:20, bottom:80, left:60};
 
 var container = d3.select(el);
 
 var containerWidth = container.node().offsetWidth;
-var containerHeight = containerWidth * 0.66;
+var containerHeight = containerWidth;
 
 var chartWidth = containerWidth - margin.right - margin.left;
 var chartHeight = containerHeight - margin.top - margin.bottom;
@@ -69,7 +69,7 @@ svg.selectAll('.bar')
       // the - 5 bumps up the text a bit so it's not directly over the bar
       var y = yScale(d[fieldname]) - 5;
       d3.select(this).classed('highlight', true);
-      tooltip.text("$".concat(d[fieldname]))
+      tooltip.text(d3.format("$,.0f")(d[fieldname]))
             .attr('transform', `translate(${x}, ${y})`)
     })
     .on('mouseleave', function(d) {
@@ -82,8 +82,90 @@ svg.selectAll('.bar')
 totalsChart("#total-raised", "total_net_contributions")
 totalsChart("#total-spent", "total_spending")
 totalsChart("#cash-on-hand", "cash_on_hand")
-//createChart("#percent-small-donors", "percent_from_small_donors")
-//createChart("#percent-large-donors", "percent_from_large_donors")
-//createChart("#percent-pacs", "percent_from_PACs")
+
+//Shows percentages for different types of fundraising
+
+function percentChart(el, fieldname) {
+
+var margin = {top: 60, right:20, bottom:80, left:60};
+
+var container = d3.select(el);
+
+var containerWidth = container.node().offsetWidth;
+var containerHeight = containerWidth;
+
+var chartWidth = containerWidth - margin.right - margin.left;
+var chartHeight = containerHeight - margin.top - margin.bottom;
+
+var svg = container.append('svg')
+            .attr('width', containerWidth)
+            .attr('height', containerHeight)
+            .append('g')
+                .attr('transform', `translate(${margin.left}, ${margin.top})`)
+var xDomain = senate_data_3.map(d => d.candidate);
+var yDomain = [0,1];
+
+var xScale = d3.scaleBand()
+              .domain(xDomain)
+              .range([0, chartWidth])
+              .padding(0.1);
+
+var yScale = d3.scaleLinear()
+              .domain(yDomain)
+              .range([chartHeight, 0]);
+
+var formatAxis = d3.format(".0%");
+
+var xAxis = d3.axisBottom(xScale);
+var yAxis = d3.axisLeft(yScale)
+              .tickFormat(formatAxis)
+              .tickSize(-chartWidth)
+              .ticks(4);
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0,${chartHeight})`)
+    .call(xAxis)
+    .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-65)");
+
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+var tooltip = svg.append('text')
+    .attr('class', 'chart-tooltip');
+
+svg.selectAll('.bar')
+    .data(senate_data_3)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', d => xScale(d.candidate))
+    .attr('y', d => yScale(d[fieldname]))
+    .attr('width', xScale.bandwidth())
+    .attr('height', d => chartHeight - yScale(d[fieldname]))
+    .on('mouseenter', function(d) {
+      // centers the text above each bar
+      var x = xScale(d.candidate) + xScale.bandwidth() / 2;
+      // the - 5 bumps up the text a bit so it's not directly over the bar
+      var y = yScale(d[fieldname]) - 5;
+      d3.select(this).classed('highlight', true);
+      tooltip.text(d3.format(".0%")(d[fieldname]))
+            .attr('transform', `translate(${x}, ${y})`)
+    })
+    .on('mouseleave', function(d) {
+      d3.select(this).classed('highlight', false);
+      tooltip.text('');
+    });
+
+}
+
+percentChart("#percent-small-donors", "percent_from_small_donors")
+percentChart("#percent-large-donors", "percent_from_large_donors")
+percentChart("#percent-pacs", "percent_from_PACs")
 
 console.log('hello, this is my charts file!');
