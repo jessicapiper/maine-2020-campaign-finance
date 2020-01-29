6a28fa62c0e23cf85b2e364e51c2f1f8562214f8
jessica piper <jpiper231@gmail.com>
2020-01-29 10:31:42 -0500

    added pie chart

--- src/_scripts/_charts.js
+++ src/_scripts/_charts.js
@@ -168,10 +168,11 @@ percentChart("#percent-small-donors", "percent_from_small_donors")
 percentChart("#percent-large-donors", "percent_from_large_donors")
 percentChart("#percent-pacs", "percent_from_PACs")
 
-function pacChart(el){
+function pacChart(el) {
 
-var margin = {top: 10, right:20, bottom:80, left:60};
+var margin = {top: 70, right:0, bottom:0, left:80};
 
+var colors = d3.scaleOrdinal(d3.schemeDark2);
 var container = d3.select(el);
 
 var containerWidth = container.node().offsetWidth;
@@ -180,27 +181,57 @@ var containerHeight = containerWidth;
 var chartWidth = containerWidth - margin.right - margin.left;
 var chartHeight = containerHeight - margin.top - margin.bottom;
 
-var svg = container.append('svg')
-            .attr('width', containerWidth)
-            .attr('height', containerHeight)
-            .append('g')
-              .attr('transform', `translate(${margin.left}, ${margin.top})`)
-var xDomain = [0,1]
+var svg = container.append("svg")
+  .attr("width",chartWidth)
+  .attr("height",chartHeight)
+  .append('g')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+var raw_data = [{type:"ideological",number:0.7501615252},{type:"leadership",number:0.02431561469},{type:"business", number:0.1746258125},{type:"other",number:0.00545860738},{type:"labor",number:0.04543844031}];
+
+var data = d3.pie().sort(null).value(function(d){return d.number;})(raw_data);
+
+var segments = d3.arc()
+  .innerRadius(2)
+  .outerRadius(90)
+  .padAngle(.05)
+  .padRadius(50)
+
+var sections = svg.append("g").attr("transform","translate(20,20)")
+  .selectAll("path").data(data);
+
+sections.enter().append("path").attr("d",segments).attr("fill",function(d){return colors(d.data.number);})
+
+var content = d3.select("g").selectAll("text").data(data);
+content.enter().append("text").each(function(d){
+  var center = segments.centroid(d);
+  d3.select(this).attr("x", center[0]).attr("y",center[1])
+                  .text(d.data.type)
+  })
+}
+
+pacChart("#collins-pacs",)
+
+/*    .domain(xDomain)
+    .range([0, chartWidth])
+    .padding(0.1);
+
+var yScale = d3.scaleLinear()
+    .domain(yDomain)
+    .range([chartHeight, 0]);
+
+var xAxis = d3.axisBottom(xScale);
+var yAxis = d3.axisLeft(yScale)
+
+var headers = ["ideological","labor","leadership","business","other"]
+
+var layers = d3.layout.stack()(headers.map(function(pac_data)))
+*/
+/*var xDomain = [0,1]
 var yDomain = pac_data.map(d => d.type);
 
 var pacData = pac_data
 
-var xScale = d3.scaleBand()
-              .domain(xDomain)
-              .range([0, chartWidth])
-              .padding(0.1);
-
-var yScale = d3.scaleLinear()
-              .domain(yDomain)
-              .range([chartHeight, 0]);
-
-var xAxis = d3.axisBottom(xScale);
-var yAxis = d3.axisLeft(yScale)
 
 svg.append("g")
     .attr("class", "x axis")
@@ -209,8 +240,9 @@ svg.append("g")
 svg.append("g")
     .attr("class", "y axis")
     .call(yAxis);
-}
+*/
 
-pacChart("#all-candidates-pacs")
+
+//pacChart("#all-candidates-pacs")
 
 console.log('hello, this is my charts file!');
